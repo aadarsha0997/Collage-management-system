3e84ee1f322fabe795603d842a4146982289f1f9
Ayush <aadarsha0997@gmail.com>
2025-11-03 19:07:47 +0545

    views for studentdashboard is created

--- person/urls.py
+++ person/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teacher/<int:id>/',views.TeacherDashboard)
+    path('teacher/<int:id>/',views.TeacherDashboard),
+    path('student/<int:id>/',views.StudentDashboard)
 ]
--- person/views.py
+++ person/views.py
@@ -9,11 +9,18 @@ from .serializers import StudentSerializer,TeacherSerializer,CitySerializer
 
 @api_view(["GET"])
 def TeacherDashboard(request,id):
-    teacher_id=id
-    teacher = Teacher.objects.prefetch_related('students').get(id=teacher_id)
+    teacher = Teacher.objects.prefetch_related('students').get(id=id) # this work same as teacher.objects.get(id=teacher id ) but difference is the performance. without this there will be 2 quairy but with this only one quiry . while fetching teacher also fetch students in one go.
     serializer=TeacherSerializer(teacher)
     content={
         'teacher': serializer.data
     }
     return Response(content)
         
+@api_view(["GET"])
+def StudentDashboard(req,id):
+    student=Student.objects.get(id=id)
+    serializer=StudentSerializer(student)
+    content={
+        "student": serializer.data
+    }
+    return Response(content)
